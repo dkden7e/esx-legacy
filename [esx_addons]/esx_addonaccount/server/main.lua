7cc2c8d47f812d2e958e70b8b4ba442eb9288888
Benito "DK_Den7e" L <benitoll1993@gmail.com>
2022-06-05 10:36:29 -0700

    Jobs Creator integration (jobs_creator)

--- [esx_addons]/esx_addonaccount/server/main.lua
+++ [esx_addons]/esx_addonaccount/server/main.lua
@@ -71,3 +71,47 @@ AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
 
 	xPlayer.set('addonAccounts', addonAccounts)
 end)
+
+
+            -- Jobs Creator integration (jobs_creator)
+            RegisterNetEvent('esx_addonaccount:refreshAccounts')
+            AddEventHandler('esx_addonaccount:refreshAccounts', function() 
+                local result = MySQL.Sync.fetchAll('SELECT * FROM addon_account')
+
+                for i=1, #result, 1 do
+                    local name   = result[i].name
+                    local label  = result[i].label
+                    local shared = result[i].shared
+
+                    local result2 = MySQL.Sync.fetchAll('SELECT * FROM addon_account_data WHERE account_name = @account_name', {
+                        ['@account_name'] = name
+                    })
+
+                    if shared == 0 then
+                        table.insert(AccountsIndex, name)
+                        Accounts[name] = {}
+
+                        for j=1, #result2, 1 do
+                            local addonAccount = CreateAddonAccount(name, result2[j].owner, result2[j].money)
+                            table.insert(Accounts[name], addonAccount)
+                        end
+                    else
+                        local money = nil
+
+                        if #result2 == 0 then
+                            MySQL.Sync.execute('INSERT INTO addon_account_data (account_name, money, owner) VALUES (@account_name, @money, NULL)', {
+                                ['@account_name'] = name,
+                                ['@money']        = 0
+                            })
+
+                            money = 0
+                        else
+                            money = result2[1].money
+                        end
+
+                        local addonAccount   = CreateAddonAccount(name, nil, money)
+                        SharedAccounts[name] = addonAccount
+                    end
+                end
+            end)
+        
